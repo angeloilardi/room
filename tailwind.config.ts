d09813c8c79874bd32059ab42142f6c999c51aff
Angelo Ilardi <ange448@gmail.com>
2024-05-04 16:04:50 +0100

    added colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'dark-gray': ' hsl(0,0%,63%)',
+        'very-dark-gray': 'hsl(0,0%,27%)'
+      }
     },
   },
   plugins: [],
